21afb94b0ad564fecb9ebd95e5ee3be5e878c1d8
Saumya Pratap Singh <soumyasingh2922@gmail.com>
2025-12-12 11:59:59 +0530

    initial commit

---
+++ app/src/main/java/com/example/ringer/BootReceiver.java
@@ -0,0 +1,23 @@
+package com.ringer.app;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent serviceIntent = new Intent(context, com.ringer.app.RingerService.class);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent);
+            } else {
+                context.startService(serviceIntent);
+            }
+        }
+    }
+}
